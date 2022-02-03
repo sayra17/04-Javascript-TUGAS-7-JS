8dc8908cd27cd911a020b0898d491ad2c8dc93ba
sayra17 <agussayra03@gmail.com>
2022-02-03 17:42:00 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+04-Javascript-TUGAS-7-JS
---
+++ tugas7.js
@@ -0,0 +1,10 @@
+var tinggi_badan = [176, 189, 178, 190, 180, 145, 187, 156, 160, 170];
+
+console.log("pengulngan biasa:")
+for (var i = 0; i < tinggi_badan.length; i++) {
+    console.log(tinggi_badan[i])
+}
+console.log("pengulangan denga FOR OF:")
+for (let x of tinggi_badan) {
+    console.log(x)
+}
